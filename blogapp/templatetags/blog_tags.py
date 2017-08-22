bc2e65c3c6d9742e9fd4e6004637488c5f921549
starFalll <fyxformal@gmail.com>
2017-08-22 15:53:47 +0800

    Auto_Create_Summery&Sum_Category_Post

--- blogapp/templatetags/blog_tags.py
+++ blogapp/templatetags/blog_tags.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from ..models import Post,Category
 from django import template
+from django.db.models.aggregates import Count
 
 register=template.Library()
 
@@ -15,4 +16,4 @@ def archives():
     
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
