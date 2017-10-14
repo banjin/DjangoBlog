329af8c0fdd016f67e5976c8ff0861bae15d04eb
starFalll <fyxformal@gmail.com>
2017-10-14 11:28:27 +0800

    Modify_words&&music

--- blogapp/static/blog/js/gallery_init.js
+++ blogapp/static/blog/js/gallery_init.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 	$('#myRoundabout').roundabout({
+		 autoplay : true,
+		 autoplayDuration :5000,
+		 autoplayPauseOnHover :true,
 		 btnNext: ".next",
 		 shape: 'tearDrop',
 		 minOpacity: 1,
-		 duration: 1
+		 duration: 1,
+		
 	});
 });
--- blogapp/tests.py
+++ blogapp/tests.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-
 # Create your tests here.
